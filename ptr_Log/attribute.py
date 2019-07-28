1295b25c3bcbc3f3aa11889321fea3b1ced0dd32
v7por9 <v7por9@yandex.com>
2019-07-28 22:29:49 +0300

    A database for Tags and URL Referencing

--- ptr_Log/attribute.py
+++ ptr_Log/attribute.py
@@ -6,8 +6,7 @@ import pymysql
 class Attributes:
     def __init__(self):
         self.url = "https://sf.co.ua"
-        self.store = []
-        self.value = ""
+        self.db_name = "ptr_Tags_DB"
         with open('st0r3.txt', 'r') as fyle:
             login_details = eval(str(fyle.readlines()).replace("\\n", ''))
             self.username = login_details[0]
@@ -30,7 +29,33 @@ class Attributes:
             return self.cursor.fetchall()
         except pymysql.err.ProgrammingError:
             print("There was an **SQL Syntax Error**")
+        return
+
+    def database_create(self):
+        """
+        To create a database of the storage of Tag names and reference URL's
+        :return:
+        """
+        log_db = list(self.action("show databases"))
+        # Eliminating unnecessary data in the list.
+        present_db = eval((str(log_db).replace("(", "").replace(",)", '')))
+        if self.db_name not in present_db:
+            self.action("create database %s" % self.db_name)
+            print("A database has been created by the name %s" % self.db_name)
+            self.action('use %s' % self.db_name)
+            # New database has been created, populate the db with tables.
+            self.action("""create table tag_Referencing
+            (id int(12) not null primary key auto_increment,
+            tags_Attributes text not null,
+            url_Referencing text)""")
+        else:
+            # Database already present, since only one database is needed.
+            print("Database already exists.")
+            pass
+        return
 
 
 test = Attributes()
-print(test.action('show database'))
+test.database_create()
+test.action('use ptr_Tags_DB')
+print(test.action("show tables"))
