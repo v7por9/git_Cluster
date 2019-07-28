59985b7e568508f4a9a6a38fc887214e7e7ec31f
v7por9 <v7por9@yandex.com>
2019-07-28 21:16:27 +0300

    Establishing SQL Query

--- ptr_Log/attribute.py
+++ ptr_Log/attribute.py
@@ -12,11 +12,25 @@ class Attributes:
             login_details = eval(str(fyle.readlines()).replace("\\n", ''))
             self.username = login_details[0]
             self.passwd = login_details[1]
-            self.connection = MySQLdb.connect('localhost', "3306", self.username, self.passwd)
+            self.connection = pymysql.connect(host='localhost',
+                                              port=3306,
+                                              user=self.username,
+                                              passwd=self.passwd)
             self.cursor = self.connection.cursor()
         return
 
-    def db_action(self, query):
-        execute = self.cursor.execute(query)
-        data = execute
-        return
+    def action(self, query):
+        """
+        :param query: Passing an SQL Command for execution.
+        :return: The result or error from the query raised.
+        """
+        # TODO Catch the Errors from the raised query.
+        try:
+            self.cursor.execute(query)
+            return self.cursor.fetchall()
+        except pymysql.err.ProgrammingError:
+            print("There was an **SQL Syntax Error**")
+
+
+test = Attributes()
+print(test.action('show database'))
