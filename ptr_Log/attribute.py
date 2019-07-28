815c76264e03fb8b7eddbd117e85dd8723cc8c00
v7por9 <v7por9@yandex.com>
2019-07-28 20:45:50 +0300

    __init__ Starting off

--- ptr_Log/attribute.py
+++ ptr_Log/attribute.py
@@ -3,3 +3,20 @@
 import os
 import pymysql
 
+class Attributes:
+    def __init__(self):
+        self.url = "https://sf.co.ua"
+        self.store = []
+        self.value = ""
+        with open('st0r3.txt', 'r') as fyle:
+            login_details = eval(str(fyle.readlines()).replace("\\n", ''))
+            self.username = login_details[0]
+            self.passwd = login_details[1]
+            self.connection = MySQLdb.connect('localhost', "3306", self.username, self.passwd)
+            self.cursor = self.connection.cursor()
+        return
+
+    def db_action(self, query):
+        execute = self.cursor.execute(query)
+        data = execute
+        return
